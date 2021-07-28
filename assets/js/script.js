3f7f0d17ebca10dd21ba3e69970133bb96171b04
Claudia Correa Salhab <claudiacorreasalhab@gmail.com>
2021-07-27 21:12:56 -0400

    fixed letter casing, data parsing, started weather icon

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,8 @@ var cityEntries;
 //[BUILD] call moment:
 var now = moment();
 
+var comboWords = "";
+
 //gets current hour from momentjs now variable
 //returns in military 24 hr time
 var hour = now.hour();
@@ -24,16 +26,9 @@ console.log("moment date: " + now.format("L"));
 //FUNCTIONS ====================================================================
 
 function searchCity(userCityInput) {
-    var lowerCaseCity = userCityInput.toLowerCase();
-    console.log("inside searchCity, lowercase: " + lowerCaseCity);
-    var initialLetterUpperCase = userCityInput.charAt(0).toUpperCase();
-    console.log("inside searchCity, initial uppercase: " + initialLetterUpperCase);
-    console.log(initialLetterUpperCase + lowerCaseCity.slice(1));
-    var initialUpperCaseCity = initialLetterUpperCase + lowerCaseCity.slice(1);
 
-    cityEntries.push({city: initialUpperCaseCity);
-    localStorage.setItem("cityEntries", JSON.stringify(cityEntries));
-
+    var properCapitalization = handleLetterCasing(userCityInput);
+    console.log("proper? " + properCapitalization);
 
     if (!userCityInput) {
         alert("You must enter a city to be able to search for its weather.");
@@ -48,15 +43,23 @@ function searchCity(userCityInput) {
         console.log("creating li now");
         var liEl = $("<li>");
         liEl.attr("class", "ui-widget-content");
-        liEl.text(initialUpperCaseCity);
+        liEl.text(properCapitalization);
         olEl.append(liEl);
 
+        var cityDateTitle = $(".cityWeatherContainer");
+        cityDateTitle.text(properCapitalization + " " + now.format("L"));
+        var weatherTop = $(".col-md-8");
+        weatherTop.css("visibility", "visible");
 
-        var baseUrl = "http://api.openweathermap.org/data/2.5/weather?";
+
+        var baseUrl = "http://api.openweathermap.org/data/2.5/weather";
         var addCity = "q=";
         var addAPIKey = "appid=aa772c06902f60c4e5f5e833c0ce31f4";
+        //https://openweathermap.org/current#data
+        //For temperature in Fahrenheit use units=imperial
+        var units = "units=imperial";
         //http://api.openweathermap.org/data/2.5/weather?q=Miami&appid=aa772c06902f60c4e5f5e833c0ce31f4
-        var constructedUrl = baseUrl + addCity + userCityInput + "&" + addAPIKey;
+        var constructedUrl = baseUrl + "?" + addCity + userCityInput + "&" + units + "&" + addAPIKey;
 
         fetch(constructedUrl, {
         method: "GET", //GET is the default.
@@ -68,19 +71,41 @@ function searchCity(userCityInput) {
         })
         .then(function (data) {
             console.log(data);
+            console.log(data.weather[0].description);
             // citySearchResults = data;
             // console.log(citySearchResults.length);
             // for (var i=0; i<citySearchResults.length; i++) {
             //     console.log(citySearchResults[i]);
             // }
+            handleWeatherIcon();
         })
         .catch( function( err ) { 
             // Error 
-        }); 
-
-        var cityDateTitle = $(".cityWeatherContainer");
-        cityDateTitle.text(initialUpperCaseCity + " " + now.format("L"));
+        });
     }
+}
+
+function handleLetterCasing(origCityCasing) {
+    
+    words = origCityCasing.split(" ");
+    //this handles capitalizing multiple words city name
+    if (words.length > 2) {
+        for (var i = 0; i < words.length; i++) {
+            words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+            comboWords = comboWords + " " + words[i];
+        }
+        return comboWords;
+    } else { //this handles capitalizing single word city name
+        var lowerCaseCity = origCityCasing.toLowerCase();
+        var initialLetterUpperCase = origCityCasing.charAt(0).toUpperCase();
+        var initialUpperCaseCity = initialLetterUpperCase + lowerCaseCity.slice(1);
+        return initialUpperCaseCity;
+    }
+}
+
+function handleWeatherIcon() {
+    //https://openweathermap.org/img/wn/04n@2x.png
+    //icon value returned goes before @
 }
 
 //USER INTERACTIONS ============================================================
